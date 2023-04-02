1be4d35dd3cc0bd652ca1de5d78e8addafbd390b
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-04-02 14:56:21 +0800

    added skills icons

---
+++ src/api/skillsApi.js
@@ -0,0 +1,57 @@
+export const mySkills = [
+  {
+    id: 1,
+    name: "HTML",
+    icon: "FaHtml5",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    icon: "FaCss3Alt",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    icon: "FaJsSquare",
+  },
+  {
+    id: 4,
+    name: "Sass",
+    icon: "FaSass",
+  },
+  {
+    id: 5,
+    name: "BEM",
+    icon: "SiBem",
+  },
+  {
+    id: 6,
+    name: "Bootstrap",
+    icon: "FaBootstrap",
+  },
+  {
+    id: 7,
+    name: "Gulp",
+    icon: "FaGulp",
+  },
+  {
+    id: 8,
+    name: "npm",
+    icon: "FaNpm",
+  },
+  {
+    id: 9,
+    name: "Git",
+    icon: "FaGit",
+  },
+  {
+    id: 10,
+    name: "React",
+    icon: "FaReact",
+  },
+  {
+    id: 11,
+    name: "Styled Components",
+    icon: "SiStyledcomponents",
+  },
+];
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,65 @@
 import React from "react";
 import { SectionHeading, Wrapper } from "../../styles/createGlobalStyle";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaSass,
+  FaBootstrap,
+  FaGulp,
+  FaNpm,
+  FaGit,
+  FaReact,
+} from "react-icons/fa";
+
+import { SiStyledcomponents, SiBem } from "react-icons/si";
+import { mySkills } from "../../api/skillsApi";
 
 export default function Skills() {
+  const renderIcon = (iconName) => {
+    switch (iconName) {
+      case "FaHtml5":
+        return <FaHtml5 />;
+      case "FaCss3Alt":
+        return <FaCss3Alt />;
+      case "FaJsSquare":
+        return <FaJsSquare />;
+      case "FaSass":
+        return <FaSass />;
+      case "SiBem":
+        return <SiBem />;
+      case "FaBootstrap":
+        return <FaBootstrap />;
+      case "FaGulp":
+        return <FaGulp />;
+      case "FaNpm":
+        return <FaNpm />;
+      case "FaGit":
+        return <FaGit />;
+      case "FaReact":
+        return <FaReact />;
+      case "SiStyledcomponents":
+        return <SiStyledcomponents />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <section id="skills">
       <Wrapper>
         <SectionHeading>Skills</SectionHeading>
+        <ul>
+          {mySkills.map((skill) => {
+            const { id, name, icon } = skill;
+            return (
+              <li key={id}>
+                {renderIcon(icon)}
+                <span>{name}</span>
+              </li>
+            );
+          })}
+        </ul>
       </Wrapper>
     </section>
   );
