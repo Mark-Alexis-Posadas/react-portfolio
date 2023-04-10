cc8db0eb451b1314be157d876c3f524a8245d49d
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2023-04-10 13:24:34 +0800

    remove slider project

--- src/api/projectsApi.js
+++ src/api/projectsApi.js
@@ -7,16 +7,9 @@ const data = [
     buttons: ["Demo", "Source"],
     tags: ["HTML", "CSS", "JavaScript"],
   },
+
   {
     id: 2,
-    pageTitle: "Slider",
-    description:
-      "This project is a simple implementation of tabs using Vanilla JavaScript. It allows you to switch between different content sections without the need for page refresh.",
-    buttons: ["Demo", "Source"],
-    tags: ["HTML", "CSS", "JavaScript"],
-  },
-  {
-    id: 3,
     pageTitle: "Accordion",
     description:
       "This is a simple accordion project built using HTML, CSS, and JavaScript.",
