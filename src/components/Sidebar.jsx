07b8ba21593180bff1f8f8cdf693249dced65c11
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-15 19:56:32 +0800

    fix sidebar bug

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,15 +10,14 @@ export default function Sidebar() {
     <aside className="w-[350px] fixed bg-slate-50 shadow-md h-full overflow-hidden left-0 top-0 bottom-0 p-10">
       <ul>
         {sidebarData.map((data, index) => (
-          <li
-            key={data.id}
-            className={`p-2 my-3 rounded flex items-center justify-between cursor-pointer ${
-              index === active ? "bg-black text-white" : "text-black"
-            }`}
-            onClick={() => setActive(index)}
-          >
-            <FontAwesomeIcon icon={data.icon} />
-            <Link to={`/${data.text === "home" ? "" : data.text}`}>
+          <li key={data.id} onClick={() => setActive(index)}>
+            <Link
+              to={`/${data.text === "home" ? "" : data.text}`}
+              className={`w-full flex items-center justify-between p-2 my-3 rounded cursor-pointer ${
+                index === active ? "text-white bg-black" : "text-black"
+              }`}
+            >
+              <FontAwesomeIcon icon={data.icon} />
               {data.text.charAt(0).toUpperCase() + data.text.slice(1)}
             </Link>
           </li>
