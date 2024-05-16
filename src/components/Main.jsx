c95fa261942baabc43a21691b7f891d50b5bb183
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-16 14:19:51 +0800

    added responsive to main and sidebar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 export default function Main({ children }) {
   return (
-    <main className="ml-[350px] w-full min-h-screen p-10">{children}</main>
+    <main className="ml-0 lg:ml-[350px] w-full min-h-screen p-3 lg:p-10">
+      {children}
+    </main>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
   const { sidebarData, active, handleSetActive } = useContext(MyContext);
 
   return (
-    <aside className="w-[350px] fixed bg-white shadow-md h-full overflow-hidden left-0 top-0 bottom-0 p-10">
+    <aside className="w-[350px] fixed bg-white shadow-md h-full overflow-hidden left-0 top-0 bottom-0 p-10 hidden lg:block">
       <ul>
         {sidebarData.map((data, index) => (
           <li key={data.id} onClick={() => handleSetActive(index)}>
@@ -17,7 +17,7 @@ export default function Sidebar() {
                 index === active ? "text-white bg-black" : "text-black"
               }`}
             >
-              <FontAwesomeIcon icon={data.icon} />
+              <FontAwesomeIcon icon={data.icon} className="hidden md:block" />
               {data.text.charAt(0).toUpperCase() + data.text.slice(1)}
             </Link>
           </li>
