5eb29ff9636dacd9d7fe1fd85ce3d80003f58ae8
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-16 19:12:17 +0800

    added back button

--- src/pages/Projects/ProjectDetail.jsx
+++ src/pages/Projects/ProjectDetail.jsx
@@ -1,6 +1,7 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useContext } from "react";
 import { MyContext } from "../../context/MyContextProvider";
+import Wrapper from "../../components/Wrapper";
 export default function ProjectDetail() {
   const { id } = useParams();
   const { projectsData } = useContext(MyContext);
@@ -11,22 +12,29 @@ export default function ProjectDetail() {
   }
 
   return (
-    <div>
-      <h1>{project.pageTitle}</h1>
-      <p>{project.description}</p>
-      <div>
-        <a href={project.demo} target="_blank" rel="noopener noreferrer">
-          Demo
-        </a>
-        <a href={project.source} target="_blank" rel="noopener noreferrer">
-          Source
-        </a>
-      </div>
-      <ul>
-        {project.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    </div>
+    <section>
+      <Wrapper>
+        <div className="flex items-center">
+          <Link to="/projects" className="mr-5">
+            <button className="bg-black rounded p-2 text-white">Back</button>
+          </Link>
+          <h1 className="font-bold">{project.pageTitle}</h1>
+        </div>
+        <p>{project.description}</p>
+        <div>
+          <a href={project.demo} target="_blank" rel="noopener noreferrer">
+            Demo
+          </a>
+          <a href={project.source} target="_blank" rel="noopener noreferrer">
+            Source
+          </a>
+        </div>
+        <ul>
+          {project.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </Wrapper>
+    </section>
   );
 }
