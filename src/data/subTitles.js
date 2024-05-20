c20573fae7df4dbfde490a167473f6515f46b6bd
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-20 14:49:59 +0800

    added comment each text subtitles text

--- src/data/subTitles.js
+++ src/data/subTitles.js
@@ -1,16 +1,19 @@
 const subTitles = [
+  //about
   { text: "about subtitle" },
-
+  //projects
   {
     text: "Take a look at the innovative projects I've brought to life. Each project showcases my ability to turn ideas into reality, demonstrating my commitment to excellence and creativity. Dive into the details of my work and see how I've made a tangible impact through these initiatives.",
   },
-
+  //skills
   {
     text: "Explore the comprehensive set of skills I've honed over the years. From technical expertise to creative problem-solving, these abilities enable me to tackle challenges and drive success. Discover how my diverse skill set can contribute to achieving remarkable results in any endeavor.",
   },
+  //experience
   {
     text: "Discover the diverse range of projects and roles I've embraced throughout my career. From leading innovative initiatives to collaborating with dynamic teams, each experience has shaped my professional journey. Explore the highlights and milestones that showcase my skills and growth.",
   },
+  //contact
   {
     text: "I'd love to hear from you! Whether you have a question, want to collaborate on a project, or just want to say hello, feel free to get in touch. Fill out the form below, and I'll get back to you as soon as I can. Your message is important to me, and I look forward to connecting with you.",
   },
