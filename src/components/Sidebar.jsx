58aea61675ac2343c069118a62ed3be808888228
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-21 15:57:55 +0800

    fix contact icon alignment in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ export default function Sidebar({
 }) {
   return (
     <aside
-      className={`md:w-[250px]  lg:w-[300px] 2xl:w-[350px] fixed bg-white shadow-md h-full overflow-hidden left-0 top-0 z-10 bottom-0 md:p-3 lg:p-10 lg:block ${
+      className={`md:w-[250px] lg:w-[300px] 2xl:w-[350px] fixed bg-white shadow-md h-full overflow-hidden left-0 top-0 z-10 bottom-0 md:p-3 lg:p-10 lg:block ${
         toggleSidebar ? "block" : "hidden"
       }`}
     >
@@ -33,7 +33,7 @@ export default function Sidebar({
         ))}
       </ul>
 
-      <ul className="flex items-center justify-center w-full gap-3 absolute bottom-0">
+      <ul className="flex items-center justify-center w-full gap-3 absolute bottom-0 left-0">
         {contactData.map((data, index) => (
           <li key={data.id} className="mb-3">
             {index === 0 ? (
