175cc4ac924ebf649f4421e2c28bf13db5084aae
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-25 22:44:36 +0800

    remove about page

--- src/data/sidebarData.js
+++ src/data/sidebarData.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 const sidebarData = [
   { id: uuidv4(), text: "home", icon: ["fas", "house-chimney"] },
-  { id: uuidv4(), text: "about", icon: ["fas", "user"] },
+  // { id: uuidv4(), text: "about", icon: ["fas", "user"] },
   { id: uuidv4(), text: "projects", icon: ["fas", "diagram-project"] },
   { id: uuidv4(), text: "skills", icon: ["fas", "user-gear"] },
   { id: uuidv4(), text: "experience", icon: ["fas", "briefcase"] },
