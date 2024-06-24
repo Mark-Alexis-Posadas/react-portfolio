ac2b0e849d8b1d720f454e7393f10d6bd58e6799
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-24 11:21:28 +0800

    added projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,100 @@
+import { v4 as uuidv4 } from "uuid";
+import { ProjectType } from "../types/projects";
+
+const projectsData: ProjectType[] = [
+  {
+    id: uuidv4(),
+    pageTitle: "Tab",
+    description:
+      "This project is a tabbed interface built with React, utilizing Tailwind CSS for styling and React Hooks for state management. The project provides a simple and intuitive way to navigate between different sections or content within a single page application.",
+    features: [
+      { text: "Tabbed Navigation" },
+      { text: "Responsive layout with Tailwind CSS" },
+      { text: "State management with React Hooks" },
+      { text: "Dynamic content rendering" },
+      { text: "Customizable styles and components" },
+    ],
+    source:
+      "https://github.com/Mark-Alexis-Posadas/react-typescript-mini-projects/blob/main/src/pages/Tab/index.tsx",
+    demo: "https://tjc6fc.csb.app/",
+    buttons: ["Demo", "Source"],
+    tags: ["react", "typescript", "tailwind CSS"],
+  },
+
+  {
+    id: uuidv4(),
+    pageTitle: "Accordion",
+
+    description:
+      "This project demonstrates an accordion component built using React, Tailwind CSS for styling, and React Hooks for state management. It allows users to expand and collapse sections to show and hide content.",
+    features: [
+      { text: "Expandable and collapsible sections" },
+      { text: "Responsive layout with Tailwind CSS" },
+      { text: "State management with React Hooks" },
+      { text: "Dynamic content rendering" },
+      { text: "Customizable styles and components" },
+    ],
+    source:
+      "https://github.com/Mark-Alexis-Posadas/react-typescript-mini-projects/blob/main/src/pages/Accordion/index.tsx",
+    demo: "https://kjky7y.csb.app/",
+    buttons: ["Demo", "Source"],
+    tags: ["react", "typescript", "tailwind CSS"],
+  },
+
+  {
+    id: uuidv4(),
+    pageTitle: "TodoList App",
+    description:
+      "This is a Todo List application built with React, Tailwind CSS, and React Hooks. Users can add, edit, and delete tasks.",
+    features: [
+      { text: "Task management (add, edit, delete)" },
+      { text: "Responsive layout with Tailwind CSS" },
+      { text: "State management with React Hooks" },
+      { text: "Dynamic content rendering" },
+      { text: "Customizable styles and components" },
+    ],
+    source:
+      "https://github.com/Mark-Alexis-Posadas/react-typescript-mini-projects/blob/main/src/pages/Todo/index.tsx",
+    demo: "https://lq6hgj.csb.app/",
+    buttons: ["Demo", "Source"],
+    tags: ["react", "typescript", "tailwind CSS"],
+  },
+
+  {
+    id: uuidv4(),
+    pageTitle: "Carousel",
+    description:
+      "This project is a carousel component built using React, Tailwind CSS, and React Hooks. It allows users to navigate through a set of images or content.",
+    features: [
+      { text: "Image/content sliding" },
+      { text: "Responsive layout with Tailwind CSS" },
+      { text: "State management with React Hooks" },
+      { text: "Customizable styles and components" },
+    ],
+    source:
+      "https://github.com/Mark-Alexis-Posadas/react-typescript-mini-projects/blob/main/src/pages/Carousel/index.tsx",
+    demo: "https://nmqnld.csb.app/",
+    buttons: ["Demo", "Source"],
+    tags: ["react", "typescript", "tailwind CSS"],
+  },
+  {
+    id: uuidv4(),
+    pageTitle: "Theme",
+    description:
+      "This project demonstrates a theme switcher built using React, Tailwind CSS, and React Hooks. It allows users to switch between different themes.",
+    features: [
+      { text: "Light/Dark mode switch" },
+      { text: "Responsive layout with Tailwind CSS" },
+      { text: "State management with React Hooks" },
+      { text: "Dynamic content rendering" },
+      { text: "Customizable styles and components" },
+    ],
+    source:
+      "https://github.com/Mark-Alexis-Posadas/react-typescript-mini-projects/blob/main/src/pages/Theme/index.tsx",
+    demo: "https://z8xq86.csb.app/",
+    buttons: ["Demo", "Source"],
+    tags: ["react", "typescript", "tailwind CSS"],
+  },
+];
+
+export default projectsData;
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,7 +1,46 @@
+import {
+  faFolder,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import projectsData from "../../data/projects";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 export default function Projects() {
   return (
     <section className="min-h-[50vh]" id="projects">
-      Projects
+      {projectsData.map((data) => (
+        <div
+          key={data.id}
+          className="bg-white shadow-custom-shadow rounded transition transform duration-300 hover:shadow-lg hover:scale-105 p-5 mb-5"
+        >
+          <header className="flex items-center justify-between">
+            <FontAwesomeIcon className="text-2xl" icon={faFolder} />
+            <div className="flex items-center gap-3">
+              <FontAwesomeIcon className="text-2xl" icon={faGithub} />
+              <FontAwesomeIcon
+                className="text-2xl"
+                icon={faArrowUpRightFromSquare}
+              />
+            </div>
+          </header>
+          <h1 className="font-bold text-xl my-3 text-black">
+            {data.pageTitle}
+          </h1>
+
+          <p className="text-sm mb-auto pb-3">{data.description}</p>
+          <ul>
+            {data.features.map((item, index) => (
+              <li key={index}>- {item.text}</li>
+            ))}
+          </ul>
+          {/* <ul className="flex items-center gap-3">
+            <FaReact className="text-2xl text-[#61DBFB]" />
+            <SiTypescript className="text-2xl text-[#007acc]" />
+            <RiTailwindCssFill className="text-2xl text-[#06b6d4]" />
+          </ul> */}
+        </div>
+      ))}
     </section>
   );
 }
