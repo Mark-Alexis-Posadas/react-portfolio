227e96046b9a1de850da2623defdc1569feede76
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2025-05-18 08:53:43 +0800

    improve projects cards design

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -10,60 +10,50 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Projects() {
   return (
     <section
-      className="grid grid-cols-1 lg:grid-cols-3 gap-3 py-10"
+      className="grid grid-cols-1 lg:grid-cols-3 gap-5 py-10"
       id="projects"
     >
-      <>
-        {projectsData.map((data) => (
-          <div
-            key={data.id}
-            className="flex flex-col rounded transition transform duration-300 hover:scale-105 p-5 dark:bg-[#1f1f1f] dark:text-[#999]  hover:bg-white hover:shadow-custom-shadow"
-          >
-            <header className="flex items-center justify-between">
-              <FontAwesomeIcon className="text-2xl" icon={faFolder} />
+      {projectsData.map((data) => (
+        <div
+          key={data.id}
+          className="flex flex-col justify-between h-50 rounded-lg transition transform duration-300 hover:scale-105 p-5 dark:bg-[#1f1f1f] dark:text-[#999] hover:bg-white hover:shadow-custom-shadow"
+        >
+          <header className="flex items-center justify-between mb-2">
+            <FontAwesomeIcon className="text-2xl" icon={faFolder} />
+            <div className="flex items-center gap-3">
+              <a href={data.source} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon className="text-2xl" icon={faGithub} />
+              </a>
+              <a href={data.demo} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon
+                  className="text-2xl"
+                  icon={faArrowUpRightFromSquare}
+                />
+              </a>
+            </div>
+          </header>
 
-              <div className="flex items-center gap-3">
-                <a href={data.source} target="_blank">
-                  <FontAwesomeIcon className="text-2xl" icon={faGithub} />
-                </a>
+          <h1 className="dark:text-white font-bold text-xl mb-2">
+            {data.pageTitle}
+          </h1>
 
-                <a href={data.demo} target="_blank">
-                  <FontAwesomeIcon
-                    className="text-2xl"
-                    icon={faArrowUpRightFromSquare}
-                  />
-                </a>
-              </div>
-            </header>
-            <h1 className="dark:text-white font-bold text-xl my-3">
-              {data.pageTitle}
-            </h1>
+          <p className="relative text-[#666] text-sm line-clamp-5 fade-after">
+            {data.description}
+          </p>
+        </div>
+      ))}
 
-            <p className="text-[#666] mb-auto pb-3">{data.description}</p>
-            {/* <ul className="text-[#666] mt-auto">
-              {data.features.map((item, index) => (
-                <li key={index}>- {item.text}</li>
-              ))}
-            </ul> */}
-            {/* <ul className="flex items-center gap-3">
-            <FaReact className="text-2xl text-[#61DBFB]" />
-            <SiTypescript className="text-2xl text-[#007acc]" />
-            <RiTailwindCssFill className="text-2xl text-[#06b6d4]" />
-          </ul> */}
-          </div>
-        ))}
-
-        <button className="flex items-center justify-center gap-3">
-          <a
-            href="https://react-typescript-projects-three.vercel.app/"
-            className="flex items-center gap-3 hover:underline"
-            target="_blank"
-          >
-            View all projects here
-            <FontAwesomeIcon icon={faArrowRight} />
-          </a>
-        </button>
-      </>
+      <button className="flex items-center justify-center gap-3 text-blue-600 dark:text-blue-400 mt-5 hover:underline">
+        <a
+          href="https://react-typescript-projects-three.vercel.app/"
+          className="flex items-center gap-3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View all projects here
+          <FontAwesomeIcon icon={faArrowRight} />
+        </a>
+      </button>
     </section>
   );
 }
