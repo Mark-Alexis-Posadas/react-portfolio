630791add3901a3c0c46a9cda7de6a7d2e88d18f
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2025-05-18 08:55:45 +0800

    fixed navbar class typo

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,7 +5,7 @@ interface NavbarProps {
 
 export default function Navbar({ navbarData }: NavbarProps) {
   return (
-    <nav className="dark:bg-blackbg-white flex items-center gap-3 fixed z-10 left-0 top-0 shadow-md md:shadow-none  w-full md:bg-inherit p-3 md:p-0 md:relative">
+    <nav className="dark:bg-black bg-white flex items-center gap-3 fixed z-10 left-0 top-0 shadow-md md:shadow-none  w-full md:bg-inherit p-3 md:p-0 md:relative">
       {navbarData.map((item, index) => (
         <li key={index} className="list-none capitalize">
           <a href={`#${item.text}`}>{item.text}</a>
